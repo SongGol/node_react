a2395f423dbd9f4e51034f60f6613d5df584750e
SongGol <dldmsgus2@gmail.com>
2022-04-01 14:09:24 +0900

    :sparkles: 로그아웃 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,26 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+    let navigate = useNavigate();
 
     useEffect(() => {
         axios.get('/api/hello')
             .then(response => console.log(response.data));
     }, []);
 
-
+    const onLogoutHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                console.log(response.data)
+                if (response.data.success) {
+                    navigate('/login');
+                } else {
+                    alert('로그아웃을 실패 했습니다.');
+                }
+            })
+    }
 
     return (
         <>
